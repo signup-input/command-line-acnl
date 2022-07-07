2326a43e70a41223c45eff5de8ddf6fff13041ef
Kaust <teamtornadoboss@gmail.com>
2022-07-07 13:32:56 -0400

    starting

--- experiments/progen.py
+++ experiments/progen.py
@@ -1,13 +1,36 @@
 '''
 Below is the pattern we will use to create the story.
 ###############################################################################
-Goal (fetch this, go here)
+Theme = mystery, adventure, exploration
+Setting (house, The Expanse, etc.)
     Goal (fetch this, go here)
         Goal (fetch this, go here)
-        method (don't kill, use bows, trade for, etc)
-        complication (only during night, use training weapons, Dire monsters, etc)
-    method (don't kill, use bows, trade for, etc)
-    complication (only during night, use training weapons, Dire monsters, etc)
-method (don't kill, use bows, trade for, etc)
-complication (only during night, use training weapons, Dire monsters, etc)
+            Goal (fetch this, go here)
+            method (don't kill, use bows, trade for, bad clues,  etc)
+            complication (only during night, use training weapons, Dire monsters, bad clues,  etc)
+        method (don't kill, use bows, trade for, bad clues,  etc)
+        complication (only during night, use training weapons, Dire monsters, bad clues,  etc)
+    method (don't kill, use bows, trade for, bad clues,  etc)
+    complication (only during night, use training weapons, Dire monsters, bad clues,  etc)
 '''
+
+if theme == "adventure":
+    if setting == "house": #main setting
+        if goal == "fetch": #main goal
+            if goal == "fetch": #sub quest/goal
+                if goal == "fetch":# sub sub quest/goal
+                    if method == "don't kill": # sub sub method                       
+                        if complication == "only during night":
+                            pass
+                    pass
+            pass
+        elif goal == "go to":
+            pass
+        pass
+    elif setting == "The Expanse":
+        pass
+    pass
+elif theme == "mystery":
+    pass
+elif theme == "exploration":
+    pass
