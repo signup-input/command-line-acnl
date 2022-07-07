2b7114fb42391bee878dbf064f5653e8dcf09eec
Kaust <teamtornadoboss@gmail.com>
2022-07-07 15:08:49 -0400

    hammering things out

--- experiments/progen.py
+++ experiments/progen.py
@@ -6,12 +6,12 @@ Setting (house, The Expanse, etc.)
     Goal (fetch this, go here)
         Goal (fetch this, go here)
             Goal (fetch this, go here)
-            method (don't kill, use bows, trade for, bad clues,  etc)
-            complication (only during night, use training weapons, Dire monsters, bad clues,  etc)
-        method (don't kill, use bows, trade for, bad clues,  etc)
-        complication (only during night, use training weapons, Dire monsters, bad clues,  etc)
-    method (don't kill, use bows, trade for, bad clues,  etc)
-    complication (only during night, use training weapons, Dire monsters, bad clues,  etc)
+            method (don't kill, use bows, trade for, etc)
+            complication (work only during night, use training weapons, Dire monsters, bad clues,  etc)
+        method (don't kill, use bows, trade for, etc)
+        complication (work only during night, use training weapons, Dire monsters, bad clues,  etc)
+    method (don't kill, use bows, trade for, etc)
+    complication (work only during night, use training weapons, Dire monsters, bad clues,  etc)
 '''
 
 import random
@@ -20,29 +20,83 @@ settings = ['The Expanse', 'Tyshau', 'your house', 'Neo-Lilar']
 goals = {'mystery': ['fetch', 'go to'],
          'adventure': ['fetch', 'go to', 'destroy'],
          'exploration': ['fetch', 'go to', 'build']}
-methods = ['don\'t kill', 'trade for', 'bad clues']
-complications = ['only during night', 'monsters', 'bad clues']
+methods = ['don\'t kill', 'trade for', 'use musical instrument']
+complications = ['work only during night', 'monsters', 'bad clues']
 
 class story():
+    global theme
     theme = random.choice(themes)
+
+    global setting
     setting = random.choice(settings)
+    settings.remove(setting)
+
     goal = random.choice(goals[theme])
     if goal == 'fetch':
-        object = random.choice(['medicine', 'food', 'gold'])
+        global item
+        item = random.choice(['medicine', 'food', 'gold'])
+        goal = 'fetch ' + item
+    elif goal == 'go to':
+        goal = 'go to ' + random.choice(settings)
+    elif goal == 'destroy':
+        creatures = ['dragon', 'monster']
+        things = ['ring', 'sheet music']
+        
+        item = random.choice([random.choice(creatures), random.choice(things)])
+        goal = 'destroy ' + item
+
     method = random.choice(methods)
     if method == 'trade for':
-        method == 'trade for' + object
+        try:
+            method ='trade for ' + item
+        except NameError:
+            method = 'trade for ' + random.choice(['medicine', 'food', 'gold'])
+    elif method == 'use musical instrument':
+        method = 'use ' + random.choice(['piano', 'guitar', 'drums', 'violin'])
+
     complication = random.choice(complications)
     def __str__(self):
-        return 'Theme: ' + self.theme + '\nSetting: ' + self.setting + '\nGoal: ' + self.goal + '\nMethod: ' + self.method + '\nComplication: ' + self.complication
+        return "-----------------------------------------------------\nTheme: " + theme + "\nSetting: " + setting + "\n-Goal: " + self.goal + "\n-Method: " + self.method + "\n-Complication: " + self.complication + "\n-----------------------------------------------------\n"
+    
     def test_story(self):
-        print("-----------------------------------------------------")
-        print(self.theme)
-        print(self.setting)
-        print(self.goal)
-        print(self.method)
-        print(self.complication)
+        print(f"""-----------------------------------------------------
+        'Tis a story of great {self.theme}.
+        You're in {self.setting}
+        and must {self.goal}
+        by {self.method}
+        {self.complication}""")
+
+class substory():
+    setting = setting
+    theme = theme
+    goal = random.choice(goals[theme])
+    if goal == 'fetch':
+        global item
+        item = random.choice(['medicine', 'food', 'gold'])
+        goal = 'fetch ' + item
+    elif goal == 'go to':
+        goal = 'go to ' + random.choice(settings)
+    method = random.choice(methods)
+    if method == 'trade for':
+        try:
+            method ='trade for ' + item
+        except NameError:
+            method = 'trade for ' + random.choice(['medicine', 'food', 'gold'])
+    elif method == 'use musical instrument':
+        method = 'use ' + random.choice(['piano', 'guitar', 'drums', 'violin']) #player should be given an instrument at this point
+    complication = random.choice(complications)
+    substory = setting + ' ' + goal + ' ' + method + ' ' + complication
+    def __str__(self):
+        self.substory = f"""-----------------------------------------------------
+        'Tis a story of great {self.theme}.
+        You're in {self.setting}
+        and must {self.goal}
+        By the way: ({self.method},
+        {self.complication})
+        -----------------------------------------------------"""
+        return self.substory
 
 story = story()
 print(story)
-story.test_story()
+substory = substory()
+print(story,substory)
