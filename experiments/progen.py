36e1333ce64698bcbf7b58c90b4b2035af1eec33
Kaust <teamtornadoboss@gmail.com>
2022-07-07 13:22:55 -0400

    procedural story for mini game

---
+++ experiments/progen.py
@@ -0,0 +1,13 @@
+'''
+Below is the pattern we will use to create the story.
+###############################################################################
+Goal (fetch this, go here)
+    Goal (fetch this, go here)
+        Goal (fetch this, go here)
+        method (don't kill, use bows, trade for, etc)
+        complication (only during night, use training weapons, Dire monsters, etc)
+    method (don't kill, use bows, trade for, etc)
+    complication (only during night, use training weapons, Dire monsters, etc)
+method (don't kill, use bows, trade for, etc)
+complication (only during night, use training weapons, Dire monsters, etc)
+'''
