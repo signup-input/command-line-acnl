ee13dcf03a177299fa43f16f29afb34d583bca79
Kayden Austin <kaydensaustin@gmail.com>
2022-07-22 16:18:53 -0400

    folder tree

--- experiments/puzzle game/main.py
+++ experiments/puzzle game/main.py
@@ -4,6 +4,12 @@ TEE = "├──"
 PIPE_PREFIX = "│   "
 SPACE_PREFIX = "    "
 
-folders = {"1": ["13","12","11"],}
+folders = {"1": ["13","12","11"],
+           "2": ["21","22","woo"],}
 
-print(f"{folders['1'][0]}\n{PIPE}{ELBOW}\n{folders['1'][1]}\n{PIPE}{ELBOW}\n{folders['1'][2]}\n{PIPE}{ELBOW}")
+#print(f"{folders['1'][0]}\n {TEE}\n{folders['1'][1]}\n {TEE}\n{folders['1'][2]}\n {TEE}")
+for folder in folders:
+    x = folder
+    print(x)
+    for folder in folders[x]:
+        print(f"{TEE}{folder}")
