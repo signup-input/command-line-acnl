b5da0cf6d8b9ec4b9468f6e193a56350fc4d2731
Kayden Austin <kaydensaustin@gmail.com>
2022-07-22 17:50:08 -0400

    crudely got each folder to open itself

--- experiments/puzzle game/gui.py
+++ experiments/puzzle game/gui.py
@@ -1,10 +1,7 @@
 import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
-
 root = tk.Tk()
-frame = Frame(root)
-
 def change_i():
     if folder_btn.image == icon:
         #start_recording()
@@ -17,22 +14,29 @@ def change_i():
         folder_btn.config(image=icon)
         folder_btn.image = icon
 
+def change_i2():
+    if folder_btn2.image == icon:
+        #start_recording()
+
+        folder_btn2.config(image=icon2)
+        folder_btn2.image = icon2
+    else:
+        #stop_recording()
+
+        folder_btn2.config(image=icon)
+        folder_btn2.image = icon
+
+
 icon = PhotoImage(file='C:/Users/AAoGr/Downloads/command-line-acnl/experiments/puzzle game/icons/icons8-file-folder-48.png') # https://icons8.com/icons/set/file-folder
 icon2 = PhotoImage(file='C:/Users/AAoGr/Downloads/command-line-acnl/experiments/puzzle game/icons/icons8-open-file-folder-48.png') # https://icons8.com/icons/set/file-folder
 
-#folder_btn = tk.Button(root, image=icon, width=70,height=60,relief=FLAT ,command=change_i )
-#folder_btn.image = icon
-#folder_btn.grid(row=0, column=1)
+#!todo: make folder_btn an object
+folder_btn = tk.Button(root, image=icon, width=70,height=60,relief=FLAT ,command=change_i )
+folder_btn.image = icon
+folder_btn.grid(row=0, column=1)
 
-rows = 10
-columns = 5
-#example values
-for x in range(rows): #rows
-    for y in range(columns): #columns
-        folder_btn = Button(root, image=icon, width=70,height=60,relief=FLAT ,command=change_i,)
-        folder_btn.image = icon
-        folder_btn.grid(column=x, row=y, sticky="news")
+folder_btn2 = tk.Button(root, image=icon, width=70,height=60,relief=FLAT ,command=change_i2 )
+folder_btn2.image = icon
+folder_btn2.grid(row=0, column=2)
 
-frame.columnconfigure(tuple(range(rows)), weight=1)
-frame.rowconfigure(tuple(range(columns)), weight=1)
 root.mainloop()
