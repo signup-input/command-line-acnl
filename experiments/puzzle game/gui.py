03acf4a489f788cbb7422c8da237d5cc16b636b6
Kayden Austin <kaydensaustin@gmail.com>
2022-07-22 18:00:59 -0400

    cleanup

--- experiments/puzzle game/gui.py
+++ experiments/puzzle game/gui.py
@@ -1,11 +1,13 @@
 import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
-root = tk.Tk()
-X = 100
-Y = 50
-root.geometry(f"{X}x{Y}")
-
+window = tk.Tk()
+X = 500
+Y = 350
+window.geometry(f"{X}x{Y}")
+#Get the current screen width and height
+#screen_width = window.winfo_screenwidth()
+#screen_height = window.winfo_screenheight()
 
 def change_i():
     if folder_btn.image == icon:
@@ -36,12 +38,12 @@ icon = PhotoImage(file='C:/Users/AAoGr/Downloads/command-line-acnl/experiments/p
 icon2 = PhotoImage(file='C:/Users/AAoGr/Downloads/command-line-acnl/experiments/puzzle game/icons/icons8-open-file-folder-48.png') # https://icons8.com/icons/set/file-folder
 
 #!todo: make folder_btn an object
-folder_btn = tk.Button(root, image=icon, width=70,height=60,relief=FLAT ,command=change_i )
+folder_btn = tk.Button(window, image=icon, width=70,height=60,relief=FLAT ,command=change_i )
 folder_btn.image = icon
 folder_btn.grid(row=0, column=1)
 
-folder_btn2 = tk.Button(root, image=icon, width=70,height=60,relief=FLAT ,command=change_i2 )
+folder_btn2 = tk.Button(window, image=icon, width=70,height=60,relief=FLAT ,command=change_i2 )
 folder_btn2.image = icon
 folder_btn2.grid(row=0, column=2)
 
-root.mainloop()
+window.mainloop()
